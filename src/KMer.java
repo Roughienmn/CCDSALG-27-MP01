9fcfe124579b3c54f5cd15174b60ef7d9cfeef50
vaerdigris <114069668+vaerdigris@users.noreply.github.com>
2022-12-06 07:54:54 +0800

    BST

---
+++ src/KMer.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class KMer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter DNA sequence S:");
+        String sequence = sc.nextLine();
+
+        System.out.println("Enter length k:");
+        int k = sc.nextInt();
+
+        BST distribution = new BST (k, sequence);
+        ArrayList<Node> substrings = distribution.getSubstrings();
+
+        System.out.println("\t" + "----------------------------");
+        System.out.println("\t" + k + "-mer  | No. of Occurrences");
+
+        System.out.println("\t" + "----------------------------");
+
+        for (int i = 0; i < substrings.size(); i++) {
+            System.out.println("\t" + substrings.get(i).getsDNA() + " | " + substrings.get(i).getOccurrences());
+        }
+
+            System.out.println("\t" + "----------------------------");
+    }
+}
