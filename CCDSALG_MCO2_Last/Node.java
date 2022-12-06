958489436ce18b2150956d6287199a0199871f2f
Maracoo <112309140+Maracoo@users.noreply.github.com>
2022-12-06 20:43:18 +0800

    Add files via upload

---
+++ CCDSALG_MCO2_Last/Node.java
@@ -0,0 +1,43 @@
+// Node contains the record of the substring (for BST):
+public class Node
+{
+    // Substring of the DNA sequence:
+    private String substring;
+    // Number of occurrences:
+    private int occurrences;
+    // Left child:
+    private Node leftChild;
+    // Right child:
+    private Node rightChild;
+
+    // Constructor:
+    public Node (String sequence){
+        this.substring = sequence;
+        this.rightChild = null;
+        this.leftChild = null;
+        this.occurrences = 1;
+    }
+
+    // Getters and Setters:
+    public String getSubstring() {
+        return substring;
+    }
+    public int getOccurrences() {
+        return occurrences;
+    }
+    public Node getLeftChild() {
+        return leftChild;
+    }
+    public Node getRightChild() {
+        return rightChild;
+    }
+    public void setOccurrences(int occurrences) {
+        this.occurrences = occurrences;
+    }
+    public void setLeftChild(Node leftChild) {
+        this.leftChild = leftChild;
+    }
+    public void setRightChild(Node rightChild) {
+        this.rightChild = rightChild;
+    }
+}
